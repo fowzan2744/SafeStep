557fc677f3748d60eacfa43fe5b2a7418599cb5b
Fowzan <153728662+fowzan2744@users.noreply.github.com>
2025-07-06 19:45:07 +0530

    api link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://safestep-backend.onrender.com/api'||'http://localhost:5000/api';
 
 // Auth endpoints
 export const AUTH_ENDPOINTS = {
